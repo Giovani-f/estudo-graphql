8a18b334451228127c88b8a3cb6da39e9c1cc0a6
Giovani <giovani@fieldcontrol.com.br>
2022-01-15 17:32:20 -0300

    feat: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module'
+  }
+}
