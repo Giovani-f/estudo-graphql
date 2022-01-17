046debce82d2323894f94a5156e91bb6899c5b53
Giovani <giovani@fieldcontrol.com.br>
2022-01-17 12:05:28 -0300

    fix: recreate-db

--- bin/recreate-db.js
+++ bin/recreate-db.js
@@ -8,10 +8,10 @@ async function run () {
     await client.query(/* sql */ `
       SELECT pg_terminate_backend(pg_stat_activity.pid)
       FROM pg_stat_activity
-      WHERE pg_stat_activity.datname in ('sammathnaur') AND pid <> pg_backend_pid();
+      WHERE pg_stat_activity.datname in ('graphql') AND pid <> pg_backend_pid();
     `)
 
-    await client.query('DROP DATABASE IF EXISTS sammathnaur;')
+    await client.query('DROP DATABASE IF EXISTS graphql;')
     await client.query('CREATE DATABASE graphql;')
     console.log('[✓] created database graphql')
   } finally {
